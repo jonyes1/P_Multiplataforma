3709b3fb0670045c6704ff5042f0fc805e122db9
jonyes1 <david.fragasva@gmail.com>
2024-09-26 23:38:42 +0100

    nomes

--- wpf_multi_1/wpf_multi_1/SecondaryWindow.xaml.cs
+++ wpf_multi_1/wpf_multi_1/SecondaryWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace wpfmulti
+namespace wpf_multi_1
 {
     /// <summary>
     /// Interação lógica para SecondaryWindow.xam
